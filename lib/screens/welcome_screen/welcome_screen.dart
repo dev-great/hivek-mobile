3c1a2ec6bfdb92ea31fb5088f1ddcc9a6a28ea52
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-01-13 01:13:28 +0100

    Update welcome_screen.dart

--- lib/screens/welcome_screen/welcome_screen.dart
+++ lib/screens/welcome_screen/welcome_screen.dart
@@ -22,109 +22,124 @@ class WelcomeScreen extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 2,
-                width: MediaQuery.of(context).size.width,
-                child: Center(
-                  child:
-                      Image.asset(AppImages.noteBook, height: 265, width: 265),
-                ),
-              ),
-              SingleChildScrollView(
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 1.7,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 2.2,
                   width: MediaQuery.of(context).size.width,
-                  decoration: const BoxDecoration(
-                      color: AppColor.whiteColor,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          topRight: Radius.circular(45))),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 10, right: 10, top: 20),
-                          child: Text(
-                            AppStrings.welcomeText,
-                            style: headerTextStyle.copyWith(
-                                fontSize: 30,
-                                color: AppColor.blackColor,
-                                decoration: TextDecoration.none,
-                                fontWeight: FontWeight.w500),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        PrimaryBtn(
-                          title: AppStrings.continueWithEmail,
-                          onPress: () {
-                            Navigator.pushNamed(context, SignUp.route);
-                          },
-                          isIcon: true,
-                          icon: Icons.mail_outlined,
-                          color: AppColor.greyColor1,
-                          iconColor: AppColor.blackColor,
-                          textColor: AppColor.blackColor,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 1,
-                              width: MediaQuery.of(context).size.width / 4,
-                              color: AppColor.greyColor,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Text(
-                                AppStrings.or,
-                                style: bodyTextStyle.copyWith(
-                                    fontSize: 14,
-                                    color: AppColor.greyColor,
-                                    fontWeight: FontWeight.normal),
-                              ),
-                            ),
-                            Container(
-                              height: 1,
-                              width: MediaQuery.of(context).size.width / 4,
-                              color: AppColor.greyColor,
-                            ),
-                          ],
-                        ),
-                        PrimaryBtn(
-                          title: AppStrings.continueWithGoogle,
-                          onPress: () {},
-                          isIcon: true,
-                          icon: Icons.facebook_rounded,
-                          color: AppColor.blueColor,
-                          iconColor: AppColor.whiteColor,
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        PrimaryBtn(
-                          title: AppStrings.continueWithApple,
-                          onPress: () {
-                            Navigator.pushNamed(context, BaseScreen.route);
-                          },
-                          isIcon: true,
-                          icon: Icons.apple,
-                          color: AppColor.blackColor,
-                          iconColor: AppColor.whiteColor,
-                        ),
-                      ],
-                    ),
+                  child: Center(
+                    child: Image.asset(AppImages.noteBook,
+                        height: 255, width: 255),
                   ),
                 ),
-              )
-            ],
+                SingleChildScrollView(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 1.8,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        color: AppColor.whiteColor,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45),
+                            topRight: Radius.circular(45))),
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, right: 20, bottom: 20),
+                        child: SingleChildScrollView(
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(
+                              maxHeight:
+                                  MediaQuery.of(context).size.height / 1.9,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 10, right: 10, top: 20),
+                                  child: Text(
+                                    AppStrings.welcomeText,
+                                    style: headerTextStyle.copyWith(
+                                        fontSize: 30,
+                                        color: AppColor.blackColor,
+                                        decoration: TextDecoration.none,
+                                        fontWeight: FontWeight.w500),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                PrimaryBtn(
+                                  title: AppStrings.continueWithEmail,
+                                  onPress: () {
+                                    Navigator.pushNamed(context, SignUp.route);
+                                  },
+                                  isIcon: true,
+                                  icon: Icons.mail_outlined,
+                                  color: AppColor.greyColor1,
+                                  iconColor: AppColor.blackColor,
+                                  textColor: AppColor.blackColor,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: 1,
+                                      width:
+                                          MediaQuery.of(context).size.width / 4,
+                                      color: AppColor.greyColor,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Text(
+                                        AppStrings.or,
+                                        style: bodyTextStyle.copyWith(
+                                            fontSize: 14,
+                                            color: AppColor.greyColor,
+                                            fontWeight: FontWeight.normal),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 1,
+                                      width:
+                                          MediaQuery.of(context).size.width / 4,
+                                      color: AppColor.greyColor,
+                                    ),
+                                  ],
+                                ),
+                                PrimaryBtn(
+                                  title: AppStrings.continueWithGoogle,
+                                  onPress: () {},
+                                  isIcon: true,
+                                  icon: Icons.facebook_rounded,
+                                  color: AppColor.blueColor,
+                                  iconColor: AppColor.whiteColor,
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                PrimaryBtn(
+                                  title: AppStrings.continueWithApple,
+                                  onPress: () {
+                                    Navigator.pushNamed(
+                                        context, BaseScreen.route);
+                                  },
+                                  isIcon: true,
+                                  icon: Icons.apple,
+                                  color: AppColor.blackColor,
+                                  iconColor: AppColor.whiteColor,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
